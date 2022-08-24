dd5cee6ad7875aea1b88063c437395756687dce8
John <johnw12688@gmail.com>
2022-08-24 21:37:32 +0700

    todo list

--- src/containers/Todo.js
+++ src/containers/Todo.js
@@ -1,15 +1,19 @@
 import { useState } from "react"
 
-export default function Todo(){
+export default function Todo() {
     const [job, setJob] = useState('')
     const [todos, setTodos] = useState([])
 
     const handleKey = (e) => {
         if (e.code === "Enter") {
-            setTodos([...todos, 
-                {title: job, status: 'CHUA_HOAN_THANH'}
+            setTodos([
+                ...todos,
+                { 
+                    title: job, 
+                    status: 'CHUA_HOAN_THANH' 
+                }
             ]) //noi gia tri mang
-            setJob('') 
+            setJob('')
         }
     }
     console.log(todos);
@@ -17,17 +21,19 @@ export default function Todo(){
         <div className="text-center items-center justify-center">
             <h1 className="text-8xl">TODO LIST</h1>
             <input className="border bg-gray-100 p-5"
-                value={job} 
+                value={job}
                 onKeyUp={handleKey}
-                onChange={(e)=>setJob(e.target.value)}
+                onChange={(e) => setJob(e.target.value)}
             />
             <div>
-                {todos.map((todo, id) => (
+                {todos.map((todo, id) => (                  
                     <div key={id}>
-                        <input 
-                            type="checkbox" 
-                            checked={todo.status === 'HOAN_THANH'} 
-                            onChange={(e)=>{}}
+                  {  console.log(id)}
+                  {console.log(todo.title)}
+                        <input
+                            type="checkbox"
+                            checked={todo.status === 'HOAN_THANH'}
+                            onChange={id === todo.title ? todo.status = 'HOAN_THANH' : todo.status = 'CHUA_HOAN_THANH'}
                         />
                         {todo.title}
                     </div>
@@ -37,4 +43,10 @@ export default function Todo(){
     )
 }
 
+// temp2.map(function(temp, index){
+//     if (index === 1){
+//         temp.status = 'HOAN_THANH'
+//     }
+//     return temp
+// })
 
